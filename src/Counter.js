32be3c23edfdf7da5419f93cfea4238a1ffefbba
Raymond Gregory <vinedsel@gmail.com>
2019-01-08 16:49:58 -0600

    Modified code to make Redux ready.

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,14 @@
 import React from 'react';
 
 class Counter extends React.Component {
-  state = { 
-      count: 0 
-    }
+
 
   increment = () => {
-    this.setState({
-      count: this.state.count + 1
-    });
+
   }
 
   decrement = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
+
   }
 
   render() {
@@ -23,7 +17,7 @@ class Counter extends React.Component {
         <h2>Counter</h2>
         <div>
           <button onClick={this.decrement}>-</button>
-          <span>{this.state.count}</span>
+          <span>{this.props.count}</span>
           <button onClick={this.increment}>+</button>
         </div>
       </div>
