b5fda1e2e0b07deb7740675c1e60a67202f7e81a
Raymond Gregory <vinedsel@gmail.com>
2019-01-09 19:34:08 -0600

    Added code to Counter file.

--- src/Counter.js
+++ src/Counter.js
@@ -1,28 +1,64 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Counter extends React.Component {
 
 
   increment = () => {
+    increment = () => {
+    this.props.dispatch({
+      type: 'Increment'
+    });
+  };
 
-  }
+  increment5 = () => {
+    increment = () => {
+    this.props.dispatch({
+      type: 'Increment5'
+    });
+  };
 
   decrement = () => {
+    this.props.dispatch({
+      type: 'Decrement'
+    });
+  };
 
-  }
+  decrement10 = () => {
+    this.props.dispatch({
+      type: 'Decrement10'
+    });
+  };
+
+  reset = () => {
+    this.props.dispatch({
+      type: 'Reset'
+    });
+  };
+
 
   render() {
     return (
       <div>
         <h2>Counter</h2>
         <div>
-          <button onClick={this.decrement}>-</button>
           <span>{this.props.count}</span>
+          <button onClick={this.decrement}>-</button>
+          <button onClick={this.decrement10}>-10</button>
           <button onClick={this.increment}>+</button>
+          <button onClick={this.increment5}>+5</button>
+          <button onClick={this.reset}>Reset</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Counter;
+
+function mapStateToProps(state) {
+return {
+  count: state.count
+  };
+}
+
+export default connect(mapStateToProps)(Counter);
